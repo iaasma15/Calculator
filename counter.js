5284b90f0377fd44818093092c361e7781591bca
asma <iaasma15@gmail.com>
2022-09-09 14:59:26 +0200

    test null,undefined, decimal numbers

--- counter.js
+++ counter.js
@@ -1,4 +1,10 @@
 export function add(firstNumber, secondNumber) {
+  if (typeof firstNumber !== "number" || typeof secondNumber !== "number") {
+    throw new Error("should pass a number");
+  }
+
   const result = firstNumber + secondNumber;
-  return result;
+  const roundedResult = Math.round(result * 1000) / 1000;
+
+  return roundedResult;
 }
--- counter.spec.js
+++ counter.spec.js
@@ -2,8 +2,33 @@ import { describe, expect, it } from "vitest";
 import { add } from "./counter";
 
 describe("counter", () => {
-  it("add two numbers", () => {
-    expect(add(5, 6)).toBe(11);
-    expect(add(2, 4)).toBe(6);
+  it("should add two numbers", () => {
+    const expectedResult = add(5, 6);
+    expect(expectedResult).toBe(11);
+  });
+
+  it("should throw an error when passing strings", () => {
+    const expectedResult = () => add("dog", "cat");
+    expect(expectedResult).toThrowError("should pass a number");
+  });
+
+  it("should throw an error when passing null", () => {
+    const expectedResult = () => add(null, null);
+    expect(expectedResult).toThrowError("should pass a number");
+  });
+
+  it("should throw an error when passing null and undefined", () => {
+    const expectedResult = () => add(null, undefined);
+    expect(expectedResult).toThrowError("should pass a number");
+  });
+
+  it("should throw an error when passing undefined", () => {
+    const expectedResult = () => add(undefined, undefined);
+    expect(expectedResult).toThrowError("should pass a number");
+  });
+
+  it("should handle decimal numbers", () => {
+    const expectedResult = add(0.1, 0.2);
+    expect(expectedResult).toBe(0.3);
   });
 });
