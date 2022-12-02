e0bd84ed408a50ccc752c51672edbade5a91153e
asma <iaasma15@gmail.com>
2022-12-02 15:27:51 +0100

    print button value on click

--- main.js
+++ main.js
@@ -1,2 +1,19 @@
 import "the-new-css-reset/css/reset.css";
 import "./styles.css";
+
+const calculatorContainer = document.querySelector(
+  "[data-calculator-container]",
+);
+
+const buttons = calculatorContainer.querySelectorAll(
+  "[data-calculator-button]",
+);
+
+for (let button of buttons) {
+  button.addEventListener("click", () => {
+    console.log(button.dataset.buttonValue);
+  });
+}
+
+//Try to make event listener for keyboard
+//use keydown
